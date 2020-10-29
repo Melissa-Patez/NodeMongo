8b051f84a22cb6fe0564c20bbe0f7db7e44c1fe1
Melissa Patez (Teltec Solutions) <m.patez@teltecsolutions.com.br>
2020-10-29 15:06:31 -0300

    Atualizado

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-//importar o express com o require
+
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
-//INICIANDO O APP .... a constante app vai excutar a funcao express
-const app =  express();
 
-//iniciando o DataBase
+const app =  express();
+app.use(express.json())
+
+
 mongoose.connect('mongodb://40.76.228.121:27017/nodeapi', {useNewUrlParser: true});
-requireDir('./src/models'); // registra o model na aplicação
+requireDir('./src/models'); 
 
 //rotas
-app.use("/api", require("./src/routes")); //toda vezque receber uma requisição apartir de uma api
+app.use("/api", require("./src/routes"));
  
 //porta do navegador
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('listent to 3000')
+});
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,13 +1,18 @@
-//aqui vai a logica das rotas
-//operaçoes de determinado model
+
 const mongoose = require("mongoose");
 const Product = mongoose.model('Product');
 
-//obj com funçoes
+
 module.exports = {
     async index(req, res) {
       const products = await Product.find(); // await vai buscar os registros no bd
 
-      return res.json(products); //json estruturas de dados facil de ler 
-    } //vai fazer a listagem de todos os produtos
+      return res.json(products);
+    }, //vai fazer a listagem de todos os produtos
+
+    async storeProduct(req, res) {
+      const {title, description, url} = req.body;
+      await Product.create({title: title, description: description, url: url});
+      return res.send(title, description, url);
+    }
 };
